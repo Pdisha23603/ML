3672a41931ef943b6b6df20daf2bced1c3e1ae3f
Patel Disha <pdisha23603@gmail.com>
2026-05-08 18:02:48 +0530

    Done
    update code

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ with open("Salary_model.pkl", "rb") as f:
 
 @app.get("/")
 def home():
-    return {"message": "Patel Disha"}
+    return {"message": "Patel Abhay manharbhai"}
 
 @app.post("/predict")
 def predict(data: dict):
